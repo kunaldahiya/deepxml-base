64f40364a36efd2f699b638bec26c745c9003903
kd <kunalsdahiya@gmail.com>
2023-11-10 23:20:45 +0530

    A: dataset class with sampling support

--- deepxml/libs/dataset_base.py
+++ deepxml/libs/dataset_base.py
@@ -131,7 +131,70 @@ class DatasetBase(torch.utils.data.Dataset):
         raise NotImplementedError("")
 
 
-class Dataset(torch.utils.data.Dataset):
+class DatasetSampling(DatasetBase):
+    """Dataset to load and use XML-Datasets
+    with shortlist
+    
+    Args:
+        data_dir (str): data directory
+        f_features (str): file containing features
+            Support for sparse, dense and sequential features
+        f_labels (str): file containing labels
+            Support for sparse or dense
+            * sparse will return just the positives
+            * dense will return all the labels as a dense array
+        f_label_features (Optional[str], optional): file containing label features.
+          Defaults to None. Support for sparse, dense and sequential features
+        data (dict, optional): preloaded features and labels.
+          Defaults to {'X': None, 'Y': None, 'Yf': None}.
+        mode (str, optional): train or test. Defaults to 'train'.
+          may be useful in cases where different things are applied 
+          to train or test set
+        sampling_params (dict, optional): Parameters for sampler. Defaults to None.
+        normalize_features (bool, optional): unit normalize? Defaults to True.
+        normalize_lables (bool, optional): inf normalize? Defaults to False.
+        feature_type (str, optional): feature type. Defaults to 'sparse'.
+        label_type (str, optional): label type. Defaults to 'dense'.
+        max_len (int, optional): max length. Defaults to -1.
+    """
+
+    def __init__(self,
+                 data_dir: str,
+                 f_features: str,
+                 f_labels: str,
+                 f_label_features: Optional[str]=None,
+                 data: dict={'X': None, 'Y': None, 'Yf': None},
+                 mode: str='train',
+                 sampling_params: dict=None,
+                 normalize_features: bool=True,
+                 normalize_lables: bool=False,
+                 feature_type: str='sparse',
+                 label_type: str='dense',
+                 max_len: int=-1,
+                 *args: Optional[Any],
+                 **kwargs: Optional[Any]) -> None:
+        super().__init__(
+            data_dir=data_dir,
+            f_features=f_features,
+            f_labels=f_labels,
+            f_label_features=f_label_features,
+            data=data,
+            mode=mode, 
+            normalize_features=normalize_features,
+            normalize_lables=normalize_lables,
+            feature_type=feature_type,
+            max_len=max_len,
+            label_type=label_type)
+        self.sampler = self.construct_sampler(sampling_params)
+
+    def construct_sampler(self, sampling_params):
+        return None
+
+    def indices_permutation(self):
+        return np.random.permutation(len(self.features))
+
+
+class DatasetTensor(torch.utils.data.Dataset):
     """Dataset for just the features
     * support for optional keyword arguments like max_len
 
