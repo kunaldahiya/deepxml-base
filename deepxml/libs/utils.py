2e80ff0694e11561a10ddcdd21f748b0b869d702
kd <kunalsdahiya@gmail.com>
2025-04-17 23:31:59 +0530

    Added helper functions

--- deepxml/libs/utils.py
+++ deepxml/libs/utils.py
@@ -4,7 +4,8 @@ from numpy import ndarray
 
 import math
 import numpy as np
-
+from contextlib import contextmanager
+from scipy.sparse import save_npz
 
 
 def compute_depth_of_tree(n: int, s: int) -> int:
@@ -32,3 +33,40 @@ def filter_predictions(pred: spmatrix, mapping: ndarray=None) -> spmatrix:
         pred[mapping[:, 0], mapping[:, 1]] = 0
         pred.eliminate_zeros()
     return pred
+
+
+def save_predictions(pred: spmatrix, fname: str) -> None:
+    save_npz(fname, pred.tocsr())
+
+
+def epochs_to_iterations(n: int, n_epochs: int, bsz: int) -> int:
+    """A helper function to convert between epoch and iterations or steps
+    * Useful for optimizer
+    
+    Args:
+        n (int): number of data points
+        n_epochs (int): number of epochs
+        bsz (int): batch size
+
+    Returns:
+        int: number of iterations or steps
+    """
+    return n_epochs * math.ceil(n//bsz)
+
+
+@contextmanager
+def evaluating(net):
+    """
+    A context manager to temporarily set the model to evaluation mode.
+    
+    It saves the current training state of the model, switches to eval mode,
+    and then restores the original state after the block is executed.
+    """
+    org_mode = net.training  # Save the current mode (True if training, False if eval)
+    net.eval()  # Set to eval mode
+    try:
+        yield net
+    finally:
+        # Restore the model's original mode
+        if org_mode:
+            net.train()
